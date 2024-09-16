f180e2f7e660dd975b0b00e77985df766237d289
Reto Kernen <Reto.Kernen@swisscom.com>
2024-09-16 16:28:59 +0200

    format files

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 #invoke guardrail module
 module "control_tower_guardrails" {
-  count                         = var.enable_guardrails_module ? 1 : 0
-  source                        = "./modules/guardrails"
-  organisational_unit_root_id   = var.organisational_unit_root_id
-  controls                      = var.controls
+  count                       = var.enable_guardrails_module ? 1 : 0
+  source                      = "./modules/guardrails"
+  organisational_unit_root_id = var.organisational_unit_root_id
+  controls                    = var.controls
 
 
 }
--- provider.tf
+++ provider.tf
@@ -8,21 +8,21 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
+
   backend "s3" {
-    bucket = "terraform-state-storage120240913135406692500000001"
-    region = "eu-central-1"
-    key    = "landingzone/terraform.tfstate"
+    bucket         = "terraform-state-storage120240913135406692500000001"
+    region         = "eu-central-1"
+    key            = "landingzone/terraform.tfstate"
     dynamodb_table = "terraform-state-storage1"
     encrypt        = true
     kms_key_id     = "alias/terraform-state-storage1"
-    profile = "Automation-Account"
+    profile        = "Automation-Account"
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-central-1"
+  region  = "eu-central-1"
   profile = "Automation-Account"
   assume_role {
     role_arn = "arn:aws:iam::268702346055:role/RoleForAutomationPipeline"
