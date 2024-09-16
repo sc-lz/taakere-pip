5476046fae49cd727f0447a564fe605388fb3f40
Reto Kernen <Reto.Kernen@swisscom.com>
2024-09-16 16:12:34 +0200

    working on import

--- import.tf
+++ import.tf
@@ -12,8 +12,3 @@ resource "aws_controltower_control" "example" {
 # arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0
 
 # terraform import aws_controltower_control.guardrails arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0
-
-import {
-  to = aws_controltower_control.guardrails
-  id = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0"
-}
