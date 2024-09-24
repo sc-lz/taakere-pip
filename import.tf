749ea3b4f87a056165a3e1ed3a9b8c1f59f65913
Reto Kernen <Reto.Kernen@swisscom.com>
2024-09-24 08:44:14 +0200

    added data source

--- import.tf
+++ import.tf
@@ -1,15 +1,18 @@
 /*
-data "aws_organizations_organization" "example" {}
+resource "aws_controltower_control" "guardrails" {
+  #provider = aws.management-account-role
+  for_each = { for control in local.guardrails_list : join(":", [keys(control)[0], values(control)[0]]) => [keys(control)[0], values(control)[0]] }
 
-resource "aws_controltower_control" "example" {
- control_identifier = "arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0"
- target_identifier  = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
+  control_identifier = each.value[0]
+
+  target_identifier = local.ous_id_to_arn_map[each.value[1]]
+
 }
 
 
-# terraform import 'aws_controltower_control.imported_controls[<control_identifier>]' <control_identifier>:<target_identifier>
+resource "aws_controltower_control" "guardrails" {
+  control_identifier = "CT.APIGATEWAY.PR.2"
+  target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
+}
 
-# arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0
-
-# terraform import aws_controltower_control.guardrails arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0
 */
--- modules/guardrails/main.tf
+++ modules/guardrails/main.tf
@@ -82,5 +82,18 @@ resource "aws_controltower_control" "guardrails" {
 
 }
 
+data "aws_controltower_controls" "this" {
+
+  target_identifier = [
+    for x in data.aws_organizations_organizational_units.root.children :
+    x.arn if x.name == "Security"
+  ][0]
+
+}
+
+
 # terraform import aws_controltower_control.guardrails arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0
 
+# List buckets: aws s3api list-buckets --profile MGM  
+
+# List controls: aws controltower list-enabled-controls --target-identifier arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro --profile MGM
