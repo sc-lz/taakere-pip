6b8fe2a82a98e626a3e4572bd441a54dd9b4f920
Reto Kernen <Reto.Kernen@swisscom.com>
2024-09-24 09:31:30 +0200

    added control enabled via console

--- import.tf
+++ import.tf
@@ -11,7 +11,7 @@ resource "aws_controltower_control" "guardrails" {
 */
 
 resource "aws_controltower_control" "guardrails" {
-  control_identifier = "arn:aws:controlcatalog:::control/b7a0rdepu2tfjgo0ddxo9waw0"
+  control_identifier = "arn:aws:controlcatalog:::control/6rilu41n0gb9w6mxrkyewoer4"
   target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
 }
 
