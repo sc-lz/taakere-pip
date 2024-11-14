2e192d34ebcbd658470b619eafd662d9eff50f9b
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2024-11-14 10:35:41 +0100

    test

--- import.tf
+++ import.tf
@@ -27,3 +27,12 @@ import {
 */
 
 
+resource "aws_controltower_control" "guardrails_v5" {
+    control_identifier = "arn:aws:controlcatalog:::control/87qo8rsoettjrxjevmjqcw1tu" #AWS-GR_EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK 
+    target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
+    }
+
+import {  
+  to = aws_controltower_control.guardrails_v5 
+  id = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/87qo8rsoettjrxjevmjqcw1tu"
+  }
