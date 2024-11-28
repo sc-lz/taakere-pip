7325fc3516992dc8fb0aba0efcb9fc42d3fd2806
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2024-11-28 14:49:27 +0100

    testing the import from tfvars file

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
-
+/*
 resource "aws_controltower_control" "guardrails_v6" {
     control_identifier = "arn:aws:controlcatalog:::control/de5sygz3v7a2rhq921s59xna6" #CT.CLOUDWATCH.PR.2
     target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
@@ -10,3 +10,4 @@ import {
   id = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controlcatalog:::control/de5sygz3v7a2rhq921s59xna6"
   }
   
+ */ 
