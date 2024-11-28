cb404bb2a2711402679c25b8f95d90c92509cc30
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2024-11-28 14:33:10 +0100

    de5sygz3v7a2rhq921s59xna6 - CT.CLOUDWATCH.PR.2

--- import.tf
+++ import.tf
@@ -1,6 +1,6 @@
 
 resource "aws_controltower_control" "guardrails_v6" {
-    control_identifier = "arn:aws:controlcatalog:::control/de5sygz3v7a2rhq921s59xna6" #AWS-GR_EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK 
+    control_identifier = "arn:aws:controlcatalog:::control/de5sygz3v7a2rhq921s59xna6" #CT.CLOUDWATCH.PR.2
     target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
     }
 
