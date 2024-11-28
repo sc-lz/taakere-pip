0e2959571c6a96726be0a12d65ca2ff3a04f67e9
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2024-11-28 15:09:40 +0100

    import with control tower

--- import.tf
+++ import.tf
@@ -1,13 +1,12 @@
-/*
 resource "aws_controltower_control" "guardrails_v6" {
     control_identifier = "arn:aws:controltower:eu-central-1::control/URQEHVTSKLLB" #CT.CLOUDWATCH.PR.2
     target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
     }
-"arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controltower:eu-central-1::control/AWS-GR_DISALLOW_VPN_CONNECTIONS"
+
 
 import {  
   to = aws_controltower_control.guardrails_v6 
   id = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro,arn:aws:controltower:eu-central-1::control/URQEHVTSKLLB"
   }
   
- */ 
+ 
