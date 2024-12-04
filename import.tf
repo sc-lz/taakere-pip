914e4e635ec28a2ea4d378a8007964f4e46041cd
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2024-12-04 15:40:11 +0100

    Importing 3 guardrails with for_each loop

--- import.tf
+++ import.tf
@@ -1,6 +1,6 @@
 
 //This code works to import one guardrail at a time
-
+/*
 resource "aws_controltower_control" "guardrails_import_1" {
     control_identifier = "arn:aws:controltower:eu-central-1::control/OBZIVWNWNIFK" 
     target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
@@ -68,4 +68,48 @@ resource "aws_controltower_control" "import_guardrails_with_import" {
 }
 */
 
+locals {
+  guardrails = [
+    {
+      name               = "guardrails_import_1"
+      control_identifier = "arn:aws:controltower:eu-central-1::control/OBZIVWNWNIFK"
+    },
+    {
+      name               = "guardrails_import_2"
+      control_identifier = "arn:aws:controltower:eu-central-1::control/AWS-GR_CLOUDTRAIL_CHANGE_PROHIBITED"
+    },
+    {
+      name               = "guardrails_import_3"
+      control_identifier = "arn:aws:controltower:eu-central-1::control/AWS-GR_CLOUDTRAIL_ENABLED"
+    }
+    # Add more guardrails here
+  ]
+}
 
+variable "ou_arn" {
+  type    = string
+  default = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
+}
+
+resource "aws_controltower_control" "import_guardrails" {
+  for_each = { for idx, guardrail in local.guardrails : guardrail.name => guardrail }
+
+  control_identifier = each.value.control_identifier
+  target_identifier  = var.ou_arn
+}
+
+# Dynamic import blocks
+resource "null_resource" "import_guardrails" {
+  for_each = { for idx, guardrail in local.guardrails : guardrail.name => guardrail }
+
+  provisioner "local-exec" {
+    command = <<EOT
+cat <<EOF >> import.tf
+import {
+  to = aws_controltower_control.import_guardrails["${each.key}"]
+  id = "${var.ou_arn},${each.value.control_identifier}"
+}
+EOF
+EOT
+  }
+}
