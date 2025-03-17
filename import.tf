24e56a19b44dc77ba421020a0794cabb81183429
Kousheka Boopathi <kousheka.boopathi@swisscom.com>
2025-03-17 11:26:48 +0100

    CT.APIGATEWAY.PR.2

--- import.tf
+++ import.tf
@@ -27,9 +27,11 @@ locals {
     guardrail_7 = {
       control_identifier = "arn:aws:controltower:eu-central-1::control/PEIQUNSHAALC" //CT.APIGATEWAY.PR.2
     }
+    /*
     guardrail_8 = {
       control_identifier = "arn:aws:controltower:eu-central-1::control/TAYPGUHJACMN" //CT.APIGATEWAY.PR.3
     }
+    */
   }
 
 }
